8ae2bb97a486a7cc4bdb04028e0bf63c929b096c
krunaal <38702900+krunaal@users.noreply.github.com>
2018-06-08 19:13:50 -0700

    testing commit

--- evorg.py
+++ evorg.py
@@ -2,6 +2,7 @@ import sys
 
 #"hello how are you.1"
 # just checking how to eddit in github
+# Just editing
 
 class NewEvent:
     def __init__(self, date, starttime, venue, serial_n=1, endtime=None):
